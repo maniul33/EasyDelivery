5d2467054fb9d9aa4dc901b1a38148a14ddc8103
Maniul Islam <maniulislam38@gmail.com>
2024-04-22 11:40:35 +0600

    id generator created

---
+++ idGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyDelivery
+{
+    public class idGenerator
+    {
+        public string createNewID(List<string> ids, string role)
+        {
+            Random r = new Random();
+            List<int> idsInt = ids.ConvertAll(s => int.Parse(s.Substring(3)));
+
+            int id;
+            do
+            {
+                id = r.Next(100000, 1000000);
+            }
+            while (idsInt.Contains(id));
+
+            return role + id;
+        }
+    }
+}
