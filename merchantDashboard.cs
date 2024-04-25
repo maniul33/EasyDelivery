7193134d4a6f6d5e3d7fee13c2e24a94f947c968
Maniul Islam <maniulislam38@gmail.com>
2024-04-25 18:52:37 +0600

    designing merchant dashboard ui

--- merchantDashboard.cs
+++ merchantDashboard.cs
@@ -19,7 +19,7 @@ namespace EasyDelivery
 
         private void merchantDashboard_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,7 +29,42 @@ namespace EasyDelivery
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newDeliveryButton_Click(object sender, EventArgs e)
+        {
+            panel8.Visible = false;
         }
     }
 }
