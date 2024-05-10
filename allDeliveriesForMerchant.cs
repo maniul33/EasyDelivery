abf2182ecfe304e83d0100b44dea389da37e7ff0
Maniul Islam <maniulislam38@gmail.com>
2024-05-10 17:49:21 +0600

    all deliveries for mmerchant done

--- allDeliveriesForMerchant.cs
+++ allDeliveriesForMerchant.cs
@@ -12,9 +12,21 @@ namespace EasyDelivery
 {
     public partial class allDeliveriesForMerchant : Form
     {
-        public allDeliveriesForMerchant()
+        public allDeliveriesForMerchant(string store_id)
         {
             InitializeComponent();
+
+            // Create an instance of the panelCreation class
+            panelCreation p = new panelCreation();
+
+            // Load delivery details into panels
+            List<Panel> panels = p.LoadDeliveryDetails(store_id);
+
+            // Add each panel created by panelCreation to the form's Controls collection
+            foreach (Panel panel in panels)
+            {
+                rightPanel.Controls.Add(panel);
+            }
         }
 
         private void allDeliveriesForMerchant_Load(object sender, EventArgs e)
@@ -23,6 +35,26 @@ namespace EasyDelivery
         }
 
         private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel9_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rightPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
